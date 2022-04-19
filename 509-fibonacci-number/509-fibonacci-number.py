580ab392b798a27261ef93b6a7f1a7361f314106
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-04-18 23:56:02 -0700

    Time: 32 ms (87.93%), Space: 13.9 MB (57.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        cache = {}
+        cache = {0:0, 1:1}
         
         def recur(n):
-            if n<=1:
-                return n
             if n in cache:
                 return cache[n]
             cache[n] = recur(n-1) + recur(n-2)
