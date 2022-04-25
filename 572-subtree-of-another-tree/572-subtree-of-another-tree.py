db93be1d74ac22e5398dd0fc9f51de8e5cabd7ef
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-04-24 17:12:43 -0700

    Time: 167 ms (45.75%), Space: 15.1 MB (64.85%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -5,33 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, root, output, level=0, direc='t'):
-        if root is None:
-            return
+#     def traverse(self, root, output, level=0, direc='t'):
+#         if root is None:
+#             return
         
-        output.append(str(root.val)+direc+str(level))
-        level+=1
-        self.traverse(root.left, output, level, 'l')
-        self.traverse(root.right, output, level, 'r')
+#         output.append(str(root.val)+direc+str(level))
+#         level+=1
+#         self.traverse(root.left, output, level, 'l')
+#         self.traverse(root.right, output, level, 'r')
         
-    def compare(self, root):
-        if root is None:
-            return 
+#     def compare(self, root):
+#         if root is None:
+#             return 
         
-        if (str(root.val)+'t0') == self.subRootArr[0]:
-            rootTraverse = []
-            self.traverse(root, rootTraverse)
-            if rootTraverse == self.subRootArr:
-                return True
-        left = self.compare(root.left)
-        if left:
-            return True
-        right = self.compare(root.right)
-        if right:
-            return True
+#         if (str(root.val)+'t0') == self.subRootArr[0]:
+#             rootTraverse = []
+#             self.traverse(root, rootTraverse)
+#             if rootTraverse == self.subRootArr:
+#                 return True
+#         left = self.compare(root.left)
+#         if left:
+#             return True
+#         right = self.compare(root.right)
+#         if right:
+#             return True
     
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.subRootArr = []
-        self.traverse(subRoot, self.subRootArr)
+#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+#         self.subRootArr = []
+#         self.traverse(subRoot, self.subRootArr)
         
-        return self.compare(root)
+#         return self.compare(root)
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t: return True
+        if not s: return False
+        
+        if self.sameTree(s, t):
+            return True
+        return (self.isSubtree(s.left, t) or
+                self.isSubtree(s.right, t))
+    
+    def sameTree(self, s, t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return (self.sameTree(s.left, t.left) and
+                    self.sameTree(s.right, t.right))
+        return False
+        
