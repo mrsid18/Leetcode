6df565d4263f1c6d5c4ffc9240bcb9c5495fef8e
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-04-27 23:13:32 -0700

    Time: 209 ms (5.04%), Space: 14.4 MB (68.41%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,12 +3,23 @@ class Solution:
         
         if len(s) != len(t):
             return False
-        inpHash = {}
+        
         for c in s:
-            inpHash[c] = inpHash.get(c, 0) + 1
-            
-        for c in t:
-            if inpHash.get(c, 0) < 1:
-                return False
-            inpHash[c] -= 1
+            if c in t:
+                idx = t.index(c)
+                t = t[:idx] + t[idx+1:]
+        
+        if len(t):
+            return False
         return True
+#         inpHash = {}
+#         for c in s:
+#             inpHash[c] = inpHash.get(c, 0) + 1
+            
+#         for c in t:
+#             if inpHash.get(c, 0) < 1:
+#                 return False
+#             inpHash[c] -= 1
+#         return True
+
+        
