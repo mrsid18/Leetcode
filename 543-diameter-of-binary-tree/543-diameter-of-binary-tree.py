2384685682e53c8d183083d5364b37303fcaa0ce
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-04-30 16:52:17 -0700

    Time: 49 ms (77.07%), Space: 16.4 MB (44.27%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -6,18 +6,35 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.maxLen = []
-        def maxDepth(node):
-            if node is None:
-                return 0
+#         self.maxLen = []
+#         def maxDepth(node):
+#             if node is None:
+#                 return 0
             
-            return 1+max(maxDepth(node.left), maxDepth(node.right))
+#             return 1+max(maxDepth(node.left), maxDepth(node.right))
         
-        def traverse(node, length):
-            if node:
-                length = max((maxDepth(node.left)+maxDepth(node.right)), length)
-                self.maxLen.append(length)
-                traverse(node.left, length)
-                traverse(node.right, length)
-        traverse(root, 0)
-        return max(self.maxLen)
+#         def traverse(node, length):
+#             if node:
+#                 length = max((maxDepth(node.left)+maxDepth(node.right)), length)
+#                 self.maxLen.append(length)
+#                 traverse(node.left, length)
+#                 traverse(node.right, length)
+#         traverse(root, 0)
+#         return max(self.maxLen)
+          
+          self.max = 0
+          
+          def traverse(node):
+            if node is None:
+                return -1
+            left = traverse(node.left)
+            right = traverse(node.right)
+            
+            self.max = max(self.max, 2+left+right)
+            
+            return 1+max(left, right)
+        
+          traverse(root)
+          return self.max
+            
+          
