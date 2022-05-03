f7b6b7dc6f74ebca5dc21b2215486aa0cbcf6b13
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-03 14:22:53 -0700

    Time: 58 ms (64.03%), Space: 15.5 MB (19.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,10 +3,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:
             return True
-        y = re.sub('[^a-zA-Z0-9]', "", s).lower()
-        m = len(y)//2
-        l = len(y)
-        for i in range(m):
-            if ord(y[i]) - ord(y[l-i-1]) != 0:
+        s = re.sub('[^a-zA-Z0-9]', "", s).lower()
+        l = len(s)
+        for i in range(l//2):
+            if ord(s[i]) - ord(s[l-i-1]) != 0:
                 return False
         return True
