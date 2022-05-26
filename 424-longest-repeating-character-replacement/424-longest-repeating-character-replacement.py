208a6680157baa2f47ca780ad45e723c094cf4bf
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-25 22:28:13 -0700

    Time: 253 ms (25.62%), Space: 13.8 MB (99.59%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -3,10 +3,11 @@ class Solution:
         char = {}
         res =0
         l=0
-        
+        maxf=0
         for r in range(len(s)):
             char[s[r]] = 1 + char.get(s[r], 0)
-            while (r-l+1) - max(char.values())>k:
+            maxf = max(maxf, char[s[r]])
+            while (r-l+1) - maxf>k:
                 char[s[l]]-=1
                 l+=1
             res = max(res, r-l+1)
