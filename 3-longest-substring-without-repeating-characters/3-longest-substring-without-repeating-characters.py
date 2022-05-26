1c23c470d4eb03f98f1f65add1ae6b8101a6210a
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-25 19:18:01 -0700

    Time: 125 ms (30.65%), Space: 14.3 MB (5.38%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,16 +1,77 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l = 0
-        maxL = 0
-        charSet = set()
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
+        #because longest substring = consecutive, we can use sliding window method
+        if not s:
+            return 0
+        l, r = 0,1
+        maxOut = 1
+        while r<len(s):
+            while l<r and s[r] in s[l:r]:
                 l+=1
-            charSet.add(s[r])
-            maxL = max(maxL, r-l+1)
+            maxOut = max(maxOut, r-l+1)
+            print(maxOut)
             r+=1
+        
+        return maxOut
+                    
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         l = 0
+#         maxL = 0
+#         charSet = set()
+#         for r in range(len(s)):
+#             while s[r] in charSet:
+#                 charSet.remove(s[l])
+#                 l+=1
+#             charSet.add(s[r])
+#             maxL = max(maxL, r-l+1)
+#             r+=1
             
-        return maxL
+#         return maxL
         
         
