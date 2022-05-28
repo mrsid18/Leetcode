ef4cdda7cce4f595ddf3b78deac723344cdedffb
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-28 16:04:14 -0700

    Time: 255 ms (5.36%), Space: 14.1 MB (5.34%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -4,9 +4,8 @@ class Solution:
         
         subsets = []
         candidates.sort()
-        length = len(candidates)
         def dfs(i):
-            if i==length or sum(subsets)>=target:
+            if i==len(candidates) or sum(subsets)>=target:
                 if sum(subsets)==target: res.append(subsets[:])
                 return
             
@@ -14,7 +13,7 @@ class Solution:
             subsets.append(candidates[i])
             dfs(i+1)
             subsets.pop()
-            while i+1<length and candidates[i]==candidates[i+1]:
+            while i+1<len(candidates) and candidates[i]==candidates[i+1]:
                 i+=1
             dfs(i+1)
             
