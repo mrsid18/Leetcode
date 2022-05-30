47a6b08975ee3fddfc5f7398c4b76d9f01a028f5
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-29 18:27:15 -0700

    Time: 48 ms (57.83%), Space: 13.8 MB (78.88%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,53 +5,28 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        output = head
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        cur = ListNode()
+        res = cur
         while list1 and list2:
-            if list1.val>list2.val:
-                head.next = list2
-                list2 = list2.next
-            else:
-                head.next = list1
+            if list1.val<list2.val:
+                cur.next = list1
                 list1 = list1.next
-            head = head.next
-        
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next            
         if list1:
-            head.next = list1
-        
+            cur.next = list1
         if list2:
-            head.next = list2
+            cur.next = list2
         
-        return output.next
-            
-#         if list1 is None:
-#             return list2
+        return res.next
         
-#         if list2 is None:
-#             return list1
-        
-#         if list1.val < list2.val:
-#             temp = head = ListNode(list1.val)
-#             list1 = list1.next
-#         else:
-#             temp = head = ListNode(list2.val)
-#             list2 = list2.next
-        
-#         while list1 is not None and list2 is not None:
-#             if list1.val < list2.val:
-#                 temp.next = ListNode(list1.val)
-#                 list1 = list1.next
-#             else:
-#                 temp.next = ListNode(list2.val)
-#                 list2 = list2.next
-#             temp = temp.next
-        
-#         if list1 is not None: 
-#             temp.next = list1
-#         elif list2 is not None:
-#             temp.next = list2
-            
-#         return head
         
         
         
