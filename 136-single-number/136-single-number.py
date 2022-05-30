7dc5dbed50e45d76fcb455dfecd875a39a6aebbb
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-05-30 15:24:11 -0700

    Time: 207 ms (40.93%), Space: 16.6 MB (84.83%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -6,23 +6,6 @@ class Solution:
         
         return res
         
-        
-        # dicto = dict()
-        # for num in nums:
-        #     dicto[num] = dicto.get(num,0)+1
-        #     if (dicto[num]) == 2:
-        #         dicto.pop(num)
-        # return list(dicto.keys())[0]
-#         nums.sort()
-#         result=0
-#         while nums and len(nums)>1:
-#             result=nums[0]
-#             num1=nums[0]
-#             num2=nums[1]
-#             if num1 != num2:
-#                 return result
-#             nums = nums[2:]
-        
-#         return nums[0]
+       
 
         
