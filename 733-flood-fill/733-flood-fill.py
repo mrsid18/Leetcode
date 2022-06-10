108d91728bcf12299e5679339aa87ffe0338d6bb
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-06-10 11:55:30 -0700

    Time: 128 ms (25.48%), Space: 14.3 MB (13.69%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,25 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        #this is graph traversal problem
+        
+        visited = set()
+        val = image[sr][sc]
+        rows,cols = len(image), len(image[0])
+        print(rows,cols)
+        def dfs(r,c):
+            if r not in range(rows) or c not in range(cols) or (r,c) in visited or image[r][c]!=val:
+                return
+            
+            visited.add((r,c))
+            image[r][c] = newColor
+            
+            directions = [[1,0],[0,1],[-1,0],[0,-1]]
+            
+            for dr,dc in directions:
+                dfs(r+dr, c+dc)
+        
+        dfs(sr,sc)
+        return image
+                
+            
+            
