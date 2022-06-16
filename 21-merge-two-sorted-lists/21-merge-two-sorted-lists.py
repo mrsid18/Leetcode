8c40979cfcd8b37aaaf2171d9bdf24c99de1104d
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-06-16 00:13:04 -0700

    Time: 57 ms (42.72%), Space: 14 MB (31.45%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -7,6 +7,7 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         cur = ListNode()
         res = cur
+        
         while list1 and list2:
             if list1.val<list2.val:
                 cur.next = list1
@@ -14,13 +15,15 @@ class Solution:
             else:
                 cur.next = list2
                 list2 = list2.next
-            cur = cur.next            
+            cur = cur.next
+            
         if list1:
             cur.next = list1
         if list2:
             cur.next = list2
         
         return res.next
+            
         
         
         
