6731bc1a0fb2f6b0c04b7b6b21fe301966003982
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-06-18 16:54:53 -0700

    Time: 43 ms (73.72%), Space: 13.9 MB (29.12%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        matrix.reverse()
+        visited = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if (i,j) not in visited:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                    visited.add((i, j))
+                    visited.add((j, i))
+               
