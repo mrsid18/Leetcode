3f931d3d92198cb233f80a2a0153ded89114acf8
Siddhartha Mehta <siddharthamehta18@gmail.com>
2022-06-22 12:57:53 -0700

    Time: 71 ms (5.77%), Space: 13.9 MB (19.11%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,15 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # if len(nums)==2:
-        #     return max(nums)
-        cache = {0:nums[0], -1:0}
-        def res(n):
-            if n in cache:
-                return cache[n]
-            cache[n] = max(nums[n]+res(n-2), res(n-1))
-            return cache[n]
+        l, r = 0, 0
         
-        return res(len(nums)-1)
-    
-    
+        for n in nums:
+            l, r = r, max(n+l, r)
         
+        return max(l, r)
