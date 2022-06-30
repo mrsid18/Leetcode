921d959aa0a9e70907f992140b6411f1e262ea1d
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-06-29 17:46:04 -0700

    Time: 248 ms (44.63%), Space: 17.8 MB (22.92%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -5,7 +5,6 @@ class Solution:
         dp[-1] = [i for i in range(len(word2), -1, -1)]
         for i in range(len(word1), -1, -1):
             dp[i][len(word2)] = len(word1)-i
-        print(dp)
         for i in range(len(word1)-1, -1, -1):
             for j in range(len(word2)-1, -1, -1):
                 if word1[i]==word2[j]:
