54c2ce28e9f33610915516f0ea39f4430f8038d1
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-03 00:41:29 -0700

    Time: 144 ms (49.09%), Space: 14.2 MB (25.41%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -3,30 +3,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
-        half = total // 2
         
-        if len(B) < len(A):
+        totalLen = len(nums1)+len(nums2)
+        half = totalLen//2
+        
+        if len(nums2)<len(nums1):
             A, B = B, A
         
-        l, r = 0, len(A) - 1
+        l, r = 0, len(A)-1
         while True:
-            i = (l + r) // 2 # A
-            j = half - i - 2 # B
-        
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
-        
-            # partition is correct
-            if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
+            i = (l+r)//2
+            j = half - i - 2
+            
+            Aleft = A[i] if i>=0 else -inf
+            Aright = A[i+1] if i+1<len(A) else inf
+            Bleft = B[j] if j>=0 else -inf
+            Bright = B[j+1] if j+1<len(B) else inf
+            
+            if Aleft<=Bright and Bleft<=Aright:
+                if totalLen%2:
                     return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+                return (max(Aleft, Bleft)+min(Aright, Bright))/2
+            elif Aleft>Bright:
+                r = i-1
             else:
-                l = i + 1
+                l = i+1
+            
+            
