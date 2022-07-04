237c655952e57a67874944c9efc6622821c6913c
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-04 00:23:11 -0700

    Time: 163 ms (28.09%), Space: 14.1 MB (24.67%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,24 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        #brute force solution would be to take each integer and sum in pairs, upto len of array-1
+        """first of all we'll make a dfs through each candidate, 
+        from next candidate onwards we won't take the candidate that we have already taken into               account and we'll dfs through that candidate"""
         
-        res = []
-        def dfs(i, cur, total):
-            if total==target:
-                res.append(cur.copy())
-                return
-            if i>=len(candidates) or total>target:
-                return
-            
-            cur.append(candidates[i])
-            dfs(i, cur, total+candidates[i])
-            
-            cur.pop()
-            dfs(i+1, cur, total)
+        def dfs(tmp, start, end, remains):
+            if not remains:
+                ans.append(tmp[:])
+            elif remains>0:
+                for idx in range(start, end):
+                    tmp.append(candidates[idx])
+                    dfs(tmp, idx, end, remains-candidates[idx])
+                    tmp.pop()
         
-        dfs(0, [], 0)
-        return res
-            
+        ans = []
+        dfs([], 0, len(candidates), target)
         
-            
+        return ans
                 
+            
+            
+            
