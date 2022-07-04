00f97fa7f50b9f59223a33e2322b67c32a379a46
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-04 14:52:40 -0700

    Time: 85 ms (73.06%), Space: 14.1 MB (22.42%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -4,14 +4,13 @@ class Solution:
             if remains==0:
                 ans.append(tmp[:])
             elif remains>0:
-                for idx in range(start, end):
-                    if idx>start and candidates[idx]==candidates[idx-1]: 
-                        continue
-                    tmp.append(candidates[idx])
-                    dfs(tmp, idx+1, end, remains - candidates[idx])
+                for i in range(start, end):
+                    if i>start and candidates[i]==candidates[i-1]: continue
+                    tmp.append(candidates[i])
+                    dfs(tmp, i+1, end, remains-candidates[i])
                     tmp.pop()
-        
+                
         ans = []
-        candidates.sort(reverse=True)
+        candidates.sort()
         dfs([], 0, len(candidates), target)
         return ans
