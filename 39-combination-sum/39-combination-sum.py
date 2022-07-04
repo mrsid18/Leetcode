b09d06902a15b3d77e1f84fa00593bcb5f6fc3f6
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-04 14:34:26 -0700

    Time: 149 ms (35.54%), Space: 14.2 MB (25.05%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,22 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        """first of all we'll make a dfs through each candidate, 
-        from next candidate onwards we won't take the candidate that we have already taken into               account and we'll dfs through that candidate"""
-        
-        def dfs(tmp, start, end, remains):
-            if not remains:
+        def dfs(tmp, cur, end, remains):
+            if cur==end: return
+            if remains==0:
                 ans.append(tmp[:])
             elif remains>0:
-                for idx in range(start, end):
-                    tmp.append(candidates[idx])
-                    dfs(tmp, idx, end, remains-candidates[idx])
-                    tmp.pop()
-        
+                tmp.append(candidates[cur])
+                dfs(tmp, cur, end, remains-candidates[cur])
+                tmp.pop()
+                dfs(tmp, cur+1, end, remains)
         ans = []
         dfs([], 0, len(candidates), target)
-        
         return ans
-                
-            
-            
             
