29873ca87e1d1c93d7c6d66f12ee169f7b39f192
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-06 15:24:24 -0700

    Time: 90 ms (75.58%), Space: 14.4 MB (13.96%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,24 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        #this is graph traversal problem
+        #this is a graph traversal problem
+        # for every graph traversal problem, you need a visited
         
         visited = set()
-        val = image[sr][sc]
-        rows,cols = len(image), len(image[0])
-        def dfs(r,c):
-            if r not in range(rows) or c not in range(cols) or (r,c) in visited or image[r][c]!=val:
+        rows, cols = len(image), len(image[0])
+        
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or (r,c) in visited:
                 return
             
             visited.add((r,c))
-            image[r][c] = newColor
+            image[r][c]=newColor
+            directions = [[1,0], [0, 1], [-1, 0], [0, -1]]
             
-            directions = [[1,0],[0,1],[-1,0],[0,-1]]
-            
-            for dr,dc in directions:
-                dfs(r+dr, c+dc)
+            for dr, dc in directions:
+                if (r+dr) in range(rows) and (c+dc) in range(cols) and image[r+dr][c+dc]==target:
+                    dfs(r+dr, c+dc)
         
-        dfs(sr,sc)
+        target=image[sr][sc]
+        dfs(sr, sc)
         return image
-                
-            
             
