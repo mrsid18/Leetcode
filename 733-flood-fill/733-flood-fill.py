0bfed063020dc45b40f2252a448179c1f0365859
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-06 15:25:42 -0700

    Time: 93 ms (72.18%), Space: 14.3 MB (13.96%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -7,15 +7,12 @@ class Solution:
         rows, cols = len(image), len(image[0])
         
         def dfs(r, c):
-            if r not in range(rows) or c not in range(cols) or (r,c) in visited:
-                return
-            
             visited.add((r,c))
             image[r][c]=newColor
             directions = [[1,0], [0, 1], [-1, 0], [0, -1]]
             
             for dr, dc in directions:
-                if (r+dr) in range(rows) and (c+dc) in range(cols) and image[r+dr][c+dc]==target:
+                if (r+dr) in range(rows) and (c+dc) in range(cols) and image[r+dr][c+dc]==target and (r+dr, c+dc) not in visited:
                     dfs(r+dr, c+dc)
         
         target=image[sr][sc]
