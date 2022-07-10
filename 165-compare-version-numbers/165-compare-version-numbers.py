fe6d88427dd564acf5335e0da3da64eca846d779
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-10 11:59:05 -0700

    Time: 34 ms (86.61%), Space: 13.9 MB (77.48%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -3,14 +3,12 @@ class Solution:
         v1 = [int(x) for x in version1.split(".")]
         v2 = [int(x) for x in version2.split(".")]
         
-        if len(v1)<len(v2):
-            tmp = [0]*(len(v2)-len(v1))
-            v1.extend(tmp)
-        else:
-            tmp = [0]*(len(v1)-len(v2))
-            v2.extend(tmp)
+        d = len(v1)-len(v2)
+        v1 += [0]*-d
+        v2 += [0]*d
         
         for i in range(len(v1)):
             if v1[i]<v2[i]: return -1
             if v1[i]>v2[i]: return 1
+        
         return 0
