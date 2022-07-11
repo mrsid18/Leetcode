7a89107de21f3cb1d05e6792b1cc365e8acf1b2c
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-07-11 14:33:41 -0700

    Time: 8859 ms (22.54%), Space: 13.9 MB (50.90%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,28 +1,22 @@
 class Solution:
-    def dfs(self,board,index,recursionStack,word,i,j):
-        #we will do dfs from the index i,j in order to complete the word on the board
-        if index == len(word)-1:
-            return True
-        recursionStack.add((i,j))
-        result = False
-        if i > 0:
-            if (i-1,j) not in recursionStack and board[i-1][j] == word[index+1]:
-                result = result or self.dfs(board,index+1,recursionStack,word,i-1,j)
-        if i < len(board)-1:
-            if (i+1,j) not in recursionStack and board[i+1][j] == word[index+1]:
-                result = result or self.dfs(board,index+1,recursionStack,word,i+1,j)
-        if j > 0:
-            if (i,j-1) not in recursionStack and board[i][j-1] == word[index+1]:
-                result = result or self.dfs(board,index+1,recursionStack,word,i,j-1)
-        if j < len(board[0])-1:
-            if (i,j+1) not in recursionStack and board[i][j+1] == word[index+1]:
-                result = result or self.dfs(board,index+1,recursionStack,word,i,j+1)
-        recursionStack.remove((i,j))
-        return result
     def exist(self, board: List[List[str]], word: str) -> bool:
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if self.dfs(board,0,set({}),word,i,j):
-                        return True
+        
+        visited = set()
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c, i):
+            if i==len(word): return True
+            if r not in range(rows) or c not in range(cols) or (r,c) in visited or board[r][c]!=word[i]: return False
+            visited.add((r,c))
+            
+            res = dfs(r+1, c, i+1) or dfs(r, c+1, i+1) or dfs(r-1, c, i+1) or dfs(r, c-1, i+1)
+            
+            visited.remove((r,c))
+            return res
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0): return True
+            
         return False
+            
