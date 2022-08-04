6e5f517242e911abaa781b28390f82ec9e4b1e2a
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-03 20:22:12 -0700

    Time: 37 ms (85.35%), Space: 13.8 MB (99.31%) - LeetHub

--- 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.py
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.py
@@ -1,23 +1,22 @@
 class Solution:
-    def strWithout3a3b(self, a: int, b: int) -> str:
+    def strWithout3a3b(self, A: int, B: int) -> str:
+        a, b = 'a', 'b'
+        if B>A:
+            A, B = B, A
+            a, b = b, a
         
-        def dfs(A, B, a='a', b='b', res=""):
-            
-            if B>A: return dfs(B, A, 'b', 'a')
-            
-            while A>0:
-                res += a
+        res = ""
+        while A>0:
+            res += a
+            A-=1
+            if A>B:
+                res+=a
                 A-=1
-                if A>B:
-                    res+=a
-                    A-=1
-                if B>0:
-                    res+=b
-                    B-=1
+            if B>0:
+                res+=b
+                B-=1
                 
-            return res
-        
-        return dfs(a, b)
+        return res
             
                 
                 
