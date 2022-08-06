4d5346deb9333309b40ac9013f2d13422dfb96e1
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-05 22:23:21 -0700

    Time: 1932 ms (87.41%), Space: 14.5 MB (17.58%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,37 +1,38 @@
 class Solution:
     def largestVariance(self, s: str) -> int:
-        from collections import defaultdict
-
-        char_list = defaultdict(list)
-        for i,char in enumerate(s):
-            char_list[char].append(i)
+        charlist = defaultdict(list)
         
-        best_ans = 0
-        for target in char_list.keys():
-            for small_char in char_list.keys():
-                if target != small_char:
-                    char_array = char_list[small_char]
-                    target_array = char_list[target]
-                    i = 0
-                    j = 0
-                    target_count = 0
-                    char_count = 0
-                    while i < len(target_array) and j < len(char_array):
-                        if char_array[j] < target_array[i]:
-                            char_count += 1
-                            j += 1
-                            if char_count > target_count:
-                                char_count = 0
-                                target_count = 0
+        for i,c in enumerate(s):
+            charlist[c].append(i)
+        
+        res = 0
+        
+        chars = charlist.keys()
+        for target in chars:
+            for char in chars:
+                if target != char:
+                    i = j = 0
+                    charcount = targetcount = 0
+                    while i<len(charlist[target]) and j<len(charlist[char]):
+                        if charlist[char][j] < charlist[target][i]:
+                            charcount +=1
+                            j+=1
+                            
+                            if charcount>targetcount:
+                                charcount = 0
+                                targetcount = 0
                         else:
-                            target_count += 1
-							## since the most recent char is target, no need to keep small char around, 
-							## in our final answer we always assume at least one char_count because we know it's in the array
-                            if target_count-char_count==0: 
-                                target_count = 1
-                                char_count = 0
-                            best_ans = max(best_ans,target_count-max(char_count,1))
-                            i += 1
-                    if i < len(target_array):
-                        best_ans = max(best_ans,target_count-max(char_count,1)+len(target_array)-i)
-        return best_ans
+                            targetcount +=1
+                            i+=1
+                            if targetcount == charcount:
+                                charcount = 0
+                                targetcount = 0
+                                
+                            res = max(res, targetcount-max(charcount, 1))
+                        
+                    if i<len(charlist[target]):
+                        res = max(res, targetcount-max(charcount,1)+len(charlist[target])-i)
+                            
+        return res
+                                
+                            
