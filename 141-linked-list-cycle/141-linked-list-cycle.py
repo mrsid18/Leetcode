b3bb1f67521e18e8d2c06f6bd0de12b3ac02e7e2
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-18 22:15:32 -0700

    Time: 112 ms (15.44%), Space: 17.7 MB (20.91%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,9 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hmap = set()
-        while head:
-            if head in hmap: return True
-            hmap.add(head)
-            head = head.next
+        l = r = head
+        
+        while r and r.next:
+            l = l.next
+            r = r.next.next
+            if l==r: return True
+            
         return False
