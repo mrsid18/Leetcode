49dc16177c4c6934a1e99e0101dba0ce08796ec2
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-18 18:42:41 -0700

    Time: 83 ms (98.97%), Space: 24.2 MB (14.35%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -6,37 +6,33 @@
 class Solution:
     
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head.next:
-            return
+        l = r = head
         
-        p1 = head
-        p2 = head.next.next
+        while r and r.next:
+            l = l.next
+            r = r.next.next
         
-        if not (p1 and p2):
-            return
-        while p2:
-            p1 = p1.next
-            p2 = p2.next.next if p2.next else None
+        cur = l.next
+        l.next = None
         
-        temp = p1.next
-        p1.next = None
-        p1 = temp
+        #reverse the list
+        prev = None
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
         
-        #reverse a linked list
-        p2 = p1.next
-        p1.next = None
+        r = prev
+        cur = head
         
-        while p2:
-            temp = p2.next
-            p2.next = p1
-            p1 = p2
-            p2 = temp
-        temp = head.next
-        while p1:
-            temp = head.next
-            head.next = p1
-            temp1 = p1.next
-            p1.next = temp
-            head = temp
-            p1 = temp1
-        
+        while r and cur:
+            nxt = cur.next
+            rnxt = r.next
+            
+            cur.next = r
+            r.next = nxt
+            
+            cur = nxt
+            r = rnxt
+            
