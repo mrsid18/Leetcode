518c8e3645088252a15a193b6532d7db5593542d
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-18 22:58:09 -0700

    Time: 138 ms (74.67%), Space: 17.7 MB (74.44%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,33 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def merge2lists(self,l1, l2):
-        node = ListNode()
-        head = node
-        while l1 and l2:
-            if l1.val>l2.val:
-                node.next = l2
-                l2 = l2.next
-            else:
-                node.next = l1
-                l1=l1.next
-            node = node.next
-        
-        if l1:
-            node.next = l1
-        if l2:
-            node.next = l2
-        
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        h = [(l.val, idx) for idx, l in enumerate(lists) if l]
+        heapq.heapify(h)
+        head = cur = ListNode(None)
+        while h:
+            val, idx = heapq.heappop(h)
+            cur.next = ListNode(val)
+            cur = cur.next
+            node = lists[idx] = lists[idx].next
+            if node:
+                heapq.heappush(h, (node.val, idx))
         return head.next
-                
-        
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists: return 
-        while len(lists)>1:
-            lists.insert(0,self.merge2lists(lists.pop(), lists.pop()))
-        
-        return lists[0]
-            
