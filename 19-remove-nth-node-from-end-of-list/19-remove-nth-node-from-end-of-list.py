af02a9904b5830cf03c292dc7b72958c431ff29a
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-18 19:15:43 -0700

    Time: 31 ms (97.06%), Space: 13.9 MB (20.55%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,69 +5,36 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        p1,p2 = head, head.next
-        if not p2:
-            return 
-        m=0
-        odd = 0
-        while p2:
-            m+=1
-            p1 = p1.next
-            if p2.next:
-                p2 = p2.next.next
-                odd = 1
-            else:
-                p2 = None
-                odd = 0
         
-        delete = 2*m + odd - n + 1
+#         1 2 3 4 5
+#           1 2 3 4
+#         5-1+1 = 5
+#         5-2+1 = 4
+#         5-5+1 = 1
         
-        if delete<=m+1:
-            p1 = head
-            m=0
-
-        steps = delete-m-1
-        if steps<=0:
-            return head.next
-        p2 = p1.next
-        while steps>1:
-            p2 = p2.next
-            p1 = p1.next
-            steps -= 1
-    
-        p1.next = p2.next
+        #find the length of the linked list
+        #simply remove that 
+        
+        cur = head
+        length = 0
+        
+        while cur:
+            cur = cur.next
+            length+=1
+        
+        nth = length - n + 1
+        
+        if nth==1: return head.next
+        
+        i = 1
+        cur = head
+        while i!=nth-1:
+            cur = cur.next
+            i+=1
+        
+        cur.next = cur.next.next
+        
         return head
-            
-                
-        # print(m,2*m+odd,p1)
         
-#     def length(self, head: [ListNode]) -> int:
-#         index=0
-#         while head != None:
-#             head=head.next
-#             index+=1
-            
-#         return index
-    
-#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-#         length = self.length(head)
-#         index = length-n
-#         temp = head
-#         prev = head
-#         idx=0
-        
-#         if index==0:
-#             return temp.next
-    
-#         while idx<index:
-#             prev=temp
-#             temp = temp.next
-#             idx+=1
-        
-#         prev.next = temp.next
-        
-#         return head
-            
-            
         
