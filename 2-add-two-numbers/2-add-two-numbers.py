4af1ab64ff66148a2167c539ea653dfdb8de52ec
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-18 21:47:06 -0700

    Time: 114 ms (42.39%), Space: 14 MB (44.03%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,28 +5,44 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        res = ListNode()
-        cur = res
+        c = 0
+        
+        cur = ListNode()
+        res = cur
         
         while l1 and l2:
-            cur.next = ListNode((l1.val+l2.val+carry)%10)
-            carry = (l1.val+l2.val+carry)//10
+            total = l1.val + l2.val + c
+            c=0
+            if total>9:
+                total -= 10
+                c = 1
+            cur.next = ListNode(total)
+            cur = cur.next
             l1 = l1.next
             l2 = l2.next
+        
+        while l1:
+            total = l1.val + c
+            c=0
+            if total>9:
+                total -= 10
+                c = 1
+            cur.next = ListNode(total)
             cur = cur.next
+            l1 = l1.next
         
-        node = l1 or l2
-        
-        while node:
-            cur.next = ListNode((node.val+carry)%10)
-            carry = (node.val+carry)//10
-            node = node.next
+        while l2:
+            total = l2.val + c
+            c=0
+            if total>9:
+                total -= 10
+                c = 1
+            cur.next = ListNode(total)
             cur = cur.next
+            l2 = l2.next
         
-        if carry:
+        if c: 
             cur.next = ListNode(1)
         
         return res.next
-        
-        
+            
