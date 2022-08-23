d00b823e2a424e31e8f14582aa58b59f3bef2dd0
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-23 16:10:44 -0700

    Time: 179 ms (75.79%), Space: 15.2 MB (23.24%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,11 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [inf]*len(nums)
-        dp[-1] = 0
+        res = 0
+        l = r = 0
         
-        for i in range(len(nums)-2,-1,-1):
-            for j in range(1,nums[i]+1):
-                if i+j>len(nums)-1: break
-                dp[i] = min(dp[i], 1+dp[i+j])
+        while r<len(nums)-1:
+            farthest = 0
+            
+            for i in range(l, r+1):
+                farthest = max(farthest, i+nums[i])            
+            l = r+1
+            r=farthest
+            res+=1
         
-        return dp[0]
+        return res
