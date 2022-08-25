af9ed8d775e34ba61b06265b9913154d79afe6af
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-25 13:02:07 -0700

    Time: 70 ms (5.57%), Space: 13.8 MB (76.30%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,48 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self,node1, node2):
-        if node1 == node2:
-            return True
-        
-        if node1 and node2 and node1.val == node2.val:
-            return (self.dfs(node1.left, node2.left) and self.dfs(node1.right, node2.right))
-        else:
-            return False
-        
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return self.dfs(p,q)
-        que1,que2 = [], []
-        if p:
-            que1 = deque([[p,'l']])
-        if q:
-            que2 = deque([[q,'l']])
-        
-        while que1:
-            n1 = len(que1)
-            n2 = len(que2)
-            if n1!=n2:
-                return False
-            
-            for i1 in range(n1):
-                ele1,pos1 = que1.popleft()
-                ele2,pos2 = que2.popleft()
-                
-                if ele1.val != ele2.val or pos1 != pos2 :
-                    return False
-                
-                if ele1.left:
-                    que1.append([ele1.left,'l'])
-                if ele1.right:
-                    que1.append([ele1.right,'r'])
-                if ele2.left:
-                    que2.append([ele2.left,'l'])
-                if ele2.right:
-                    que2.append([ele2.right,'r'])
-                
-                
-        if que1 or que2:
-                 return False
-            
-        return True
-                
+    def isSameTree(self, n1: Optional[TreeNode], n2: Optional[TreeNode]) -> bool:
+        if n1 and n2: return n1.val==n2.val and self.isSameTree(n1.left, n2.left) and self.isSameTree(n1.right, n2.right)
+        if not n1 and not n2: return True
+        return False
