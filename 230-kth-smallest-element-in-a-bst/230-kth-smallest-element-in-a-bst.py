7df235c277ff43761c1f541b0713f0f8209dfe7a
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-08-25 15:59:18 -0700

    Time: 95 ms (28.21%), Space: 18.1 MB (45.87%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -6,40 +6,16 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # O(logn) to find an element
-        # so we need to know all the elements
-        
-        # iterative
-        cur = root
-        stack = []
-        n = 0
-        
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            
-            cur = stack.pop()
-            n+=1
-            
-            if n==k:
-                return cur.val
-            
-            cur = cur.right
-        
-        
-        # recursive
-        
         res = []
         def dfs(node):
-            if not node:
-                return 
+            if not node: return
             
-            dfs(node.left)
             res.append(node.val)
+            dfs(node.left)
             dfs(node.right)
-            
         dfs(root)
+        
+        res.sort()
         
         return res[k-1]
             
