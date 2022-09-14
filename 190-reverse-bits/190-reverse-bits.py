07aa042172357076b70120d3eca3fe69b4bc8b85
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-09-13 17:09:57 -0700

    Time: 64 ms (22.72%), Space: 13.8 MB (93.95%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -3,7 +3,7 @@ class Solution:
         res = 0
         
         for i in range(32):
-            bit = (n>>i)&1
+            bit = n>>i&1
             res = res | bit<<(31-i)
-            
+        
         return res
