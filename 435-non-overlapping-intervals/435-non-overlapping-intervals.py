fdfb00df0c1a3a37ee958559fc2bbaa12cc4e8ed
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-10-05 11:45:22 -0700

    Time: 2896 ms (36.72%), Space: 53 MB (7.46%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -1,16 +1,24 @@
+"""
+First sort the intervals according to start times. 
+Start iterating through the intervals, and the intervals once I find a intersection, remove the largest interval -> (end - start) 
+"""
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        res = 0
-        disjoint = []
-        for i in range(0, len(intervals)):
-            if disjoint and intervals[i][0]<disjoint[-1][1]:
-                res+=1
-                if intervals[i][1]<disjoint[-1][1]:
-                    disjoint.pop()
-                    disjoint.append(intervals[i])
-            else:
-                disjoint.append(intervals[i])
         
-        print(intervals, disjoint)
+        intervals.sort(key=lambda x: x[0])
+        
+        res = 0
+        
+        resInt = [intervals[0]]
+        
+        for i in range(1, len(intervals)):
+            prev_start, prev_end = resInt[-1]
+            start, end = intervals[i]
+            if start in range(prev_start, prev_end):
+                resInt[-1][1] = min(prev_end, end)
+                res+=1
+            else:
+                resInt.append(intervals[i])
+                
         return res
