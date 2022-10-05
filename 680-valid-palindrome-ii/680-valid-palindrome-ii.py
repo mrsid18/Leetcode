d4001fe53a6698ee84d959b9386216d1d2b4de46
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-10-04 19:07:38 -0700

    Time: 230 ms (56.81%), Space: 14.6 MB (18.54%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,17 +1,9 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
-        def check(s, once):
-            l, r = 0, len(s)-1
-            
-            while l<r:
-                if s[l]!=s[r]:
-                    if once: return False
-                    return check(s[l:r], True) or check(s[l+1:r+1], True)
-                else:
-                    l+=1
-                    r-=1
-                
-            return True
-        
-        return check(s, False)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                one, two = s[left:right], s[left + 1:right + 1]
+                return one == one[::-1] or two == two[::-1]
+            left, right = left + 1, right - 1
+        return True
