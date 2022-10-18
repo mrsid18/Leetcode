2ce180e0319101543dc327693a3463e42d608620
Siddharth Mehta <siddharthamehta18@gmail.com>
2022-10-18 13:31:39 -0700

    Time: 5387 ms (28.80%), Space: 14 MB (58.90%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -17,7 +17,17 @@ One way of thinking can be like
 
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [0] + [float('inf')]*n
+        def isPerfectSqr(x):
+            return x**.5 % 1 == 0
+        dp = [n]*(n+1)
+        s = []
         for i in range(1, n+1):
-            dp[i] = min(dp[i-j*j] for j in range(1, int(i**0.5)+1)) + 1
+            if isPerfectSqr(i):
+                dp[i]=1
+                s.append(i)
+            else:
+                for j in range(len(s)-1, -1, -1):
+                    dp[i] = min(dp[s[j]]+dp[i-s[j]], dp[i])
+                    if dp[i]==2: continue
+               
         return dp[n]
